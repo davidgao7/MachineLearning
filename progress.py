32eab29484534c3d3361b4eec668a9b0a9b5e45e
David Gao <582435572@qq.com>
2021-05-20 17:04:32 -0500

    visulize progress bar, useful while customize training epochs

--- progress.py
+++ progress.py
@@ -4,5 +4,10 @@ from tqdm import tqdm
 # just wrap any <iterable> with *tqdm(iterable)* , and you're done!
 # here's an example
 
+'''
+(base) tengjungao@Tengjuns-MacBook-Pro MachineLearning % python progress.py 
+100%|███████████████████████████████████████████████████████| 1000/1000 [00:00<00:00, 3248879.94it/s]
+'''
+
 for i in tqdm(range(1000)):
 	pass
