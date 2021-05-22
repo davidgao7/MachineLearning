e2215d4ff90200ba1879bf51a53e0cc9f5b0c41f
David Gao <582435572@qq.com>
2021-05-22 01:26:11 -0500

    run shell with args run shell with args!

---
+++ cmd_arg.sh
@@ -0,0 +1,10 @@
+echo "Username: $1";
+echo "Age: $2";
+echo "Full Name: $3";
+
+# how to run:
+# sh cmd_arg.sh [usr_name] [age] [full_name]
+#(base) tengjungao@Tengjuns-MacBook-Pro MachineLearning % sh cmd_arg.sh David 22 'Your Father'
+#Username: David
+#Age: 22
+#Full Name: Your Father
