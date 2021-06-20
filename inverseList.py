3f7a2fe68c1a23ce53f258a93d08c8248a5976ff
David Gao <582435572@qq.com>
2021-06-20 14:19:56 -0500

    python easy list operation

---
+++ inverseList.py
@@ -0,0 +1,13 @@
+x =  [1,2,3,4,5]
+
+# reverse the list
+print("x: ", x)
+print("x reverse:", x[::-1])
+
+# remove last element of list
+print("x: ", x)
+print("last element remove:", x[:-1])
+
+# get last element
+print("x: ", x)
+print("return last element:", x[-1])
